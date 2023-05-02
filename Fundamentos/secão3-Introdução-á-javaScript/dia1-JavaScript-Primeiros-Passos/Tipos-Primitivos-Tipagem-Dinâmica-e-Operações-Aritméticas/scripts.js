73e7be99242eb8f913d48b0a5796787f66a47f0e
Danilo gomes <danilo.gomes.dg91@gmail.com>
2023-05-02 16:04:05 -0300

    acabando a sengunda atuividade

---
+++ Fundamentos/secão3-Introdução-á-javaScript/dia1-JavaScript-Primeiros-Passos/Tipos-Primitivos-Tipagem-Dinâmica-e-Operações-Aritméticas/scripts.js
@@ -0,0 +1,8 @@
+let base = 5;
+let height = 8;
+let area = base * height;      
+let perimeter = base + height + area;
+console.log(base)
+console.log(height)
+console.log(area)
+console.log(perimeter)
