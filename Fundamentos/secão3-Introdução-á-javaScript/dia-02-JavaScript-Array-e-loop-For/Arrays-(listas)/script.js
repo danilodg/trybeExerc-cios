c857e0a22168d7352475e49009dfa4f608711044
Danilo gomes <danilo.gomes.dg91@gmail.com>
2023-05-03 14:16:05 -0300

    acabdno a atividade de arrays

--- Fundamentos/secão3-Introdução-á-javaScript/dia-02-JavaScript-Array-e-loop-For/Arrays-(listas)/script.js
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-02-JavaScript-Array-e-loop-For/Arrays-(listas)/script.js
@@ -1,4 +1,11 @@
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices ;
+let menuServices = menu[1];
 
-console.log(menu[1]);
+console.log(menuServices);
+
+let indexOfPortfolio = menu.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+menu.push("Contato");
+console.log(menu);
