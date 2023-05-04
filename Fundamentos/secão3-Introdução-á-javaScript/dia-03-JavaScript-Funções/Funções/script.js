06b6697751f59133e542605ce748781dfecb4592
danilo <danilo.gomes.dg91@gmail.com>
2023-05-04 20:23:43 -0300

    aquivos completos

---
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-03-JavaScript-Funções/Funções/script.js
@@ -0,0 +1,18 @@
+let saldo = 1100;
+
+function multiplicaSaldo(valor) {
+    return valor * saldo;
+};
+function divideSaldo(valor) {
+    return saldo / valor;
+};
+function somaSaldo(valor) {
+    return valor + saldo;
+};
+function subtraiSaldo(valor) {
+    return saldo - valor; 
+};
+console.log(somaSaldo(700));
+console.log(divideSaldo(10));
+console.log(subtraiSaldo(100));
+console.log(multiplicaSaldo(4));
