5ff57cf346fb25198b58dfc6a2426111894b94a2
danilo <danilo.gomes.dg91@gmail.com>
2023-05-05 18:45:52 -0300

    acabando a quinta exercicos completo

---
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-04-JavaScript-Objetos/Exercícios/script4.js
@@ -0,0 +1,18 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+  
+  info ['recorrente'] = 'Sim'
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origen: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: 'Sim',
+  }; 
+
+  for (let index in info, info2){
+    console.log(info[index] + ' e ' + info2[index]);
+  }
