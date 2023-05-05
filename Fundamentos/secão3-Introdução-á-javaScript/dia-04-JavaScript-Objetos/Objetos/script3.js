e1eeababad4353ecb32e61664cc34f2d0f481cbd
danilo <danilo.gomes.dg91@gmail.com>
2023-05-05 14:31:01 -0300

    quarta atividade completa

---
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-04-JavaScript-Objetos/Objetos/script3.js
@@ -0,0 +1,9 @@
+let player = {
+   medalhas: {
+   ouro: 2,
+   prata: 3,
+   } 
+};
+
+console.log("A jogadora possui " + player.medalhas["ouro"] + " medalhas de ouro e " + player.medalhas["prata"] + " medalhas de prata" );
+
