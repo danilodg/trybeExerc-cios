7c42c3f05ceee710c7eb1ad481674380f121dda6
danilo <danilo.gomes.dg91@gmail.com>
2023-05-05 13:49:18 -0300

    primeira atividade completa

--- Fundamentos/secão3-Introdução-á-javaScript/dia-04-JavaScript-Objetos/Objetos/script.js
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-04-JavaScript-Objetos/Objetos/script.js
@@ -3,3 +3,10 @@ let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
 
+let meuObjeto = {
+    name: "Marta",
+    lastName: "Silva",
+    age: 34,
+};
+
+console.log("A jogadora " + meuObjeto.name + " tem " + meuObjeto.age + "anos de idade")
