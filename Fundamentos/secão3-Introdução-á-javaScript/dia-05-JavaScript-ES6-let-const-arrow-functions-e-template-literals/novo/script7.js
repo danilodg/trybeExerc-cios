1772051518a1e274bffe7fbd105ec50ef12de84a
danilo <danilo.gomes.dg91@gmail.com>
2023-05-08 19:36:21 -0300

    acabando atividade

---
+++ Fundamentos/secão3-Introdução-á-javaScript/dia-05-JavaScript-ES6-let-const-arrow-functions-e-template-literals/novo/script7.js
@@ -0,0 +1,9 @@
+  let speed = 90;
+  const speedCar = (speed => 120) ? 'Você ultrapassou o limite de velocidade' : 'Você está na velocidade permitida'
+  //  if (speed >= 120) {
+  //    return `Você ultrapassou o limite de velocidade`;
+  //  } else {
+  //    return `Você está na velocidade permitida`;
+  //  }
+
+  console.log(speedCar);
